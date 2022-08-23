935e3c0d5355e368635302b79050321312297bc8
Anzelo7 <andrea.anzellotti7@gmail.com>
2022-08-23 19:35:11 +0200

    modifica

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ const textage = prompt('Qual è la tua età?');
 const textageAsN = parseInt(textage);
 console.log('textageAsN', textageAsN);
 
-if (textage < 18) {
+if (textageAsN < 18) {
     //applicare discount del 20% ai minorenni
 
     const calckmtot = calckm - 20 % ;
 
-} else if (textage > 65) {
+} else if (textageAsN > 65) {
     //applicare discount del 40% ai maggiori di 65 anni
 
     const calckmtot = calckm - 40 % ;
