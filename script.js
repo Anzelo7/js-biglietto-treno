4e19a89a24583f9857d8e2c15515dfe574179e25
Anzelo7 <andrea.anzellotti7@gmail.com>
2022-08-23 19:33:20 +0200

    calcolo discount

--- script.js
+++ script.js
@@ -12,15 +12,15 @@ console.log('textageAsN', textageAsN);
 if (textage < 18) {
     //applicare discount del 20% ai minorenni
 
-    const calckmtot = calckm - 20 %
+    const calckmtot = calckm - 20 % ;
 
 } else if (textage > 65) {
-    //applicare discount del 20% ai maggiori di 65 anni
+    //applicare discount del 40% ai maggiori di 65 anni
 
-    const calckmtot = calckm - 40 %
+    const calckmtot = calckm - 40 % ;
 }
 
 
-const rispostakm = `il prezzo è di ${calckmtot}`;
+const rispostakm = `Il prezzo è di ${calckmtot}`;
 
 document.getElementById('train_id').innerHTML = rispostakm;
