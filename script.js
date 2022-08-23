fe24a54dbd34c55f1b1e6299a8939cb05581f617
Anzelo7 <andrea.anzellotti7@gmail.com>
2022-08-23 19:24:07 +0200

    calcolo km totali

--- script.js
+++ script.js
@@ -3,17 +3,24 @@ const textkm = prompt('Quanti chilometri vuoi percorrere?');
 const textkmAsN = parseInt(textkm);
 console.log('textkmAsN', textkmAsN);
 const calckm = textkmAsN * 0.21;
-const rispostakm = `il prezzo è di ${calckm}`;
 
-document.getElementById('train_id').innerHTML = rispostakm;
 
 const textage = prompt('Qual è la tua età?');
+const textageAsN = parseInt(textage);
+console.log('textageAsN', textageAsN);
 
-
-/*
 if (textage < 18) {
-    add 20 % discount
+    //applicare discount del 20% ai minorenni
+
+    const calckmtot = calckm - 20 %
+
 } else if (textage > 65) {
-    add 40 % discount
+    //applicare discount del 20% ai maggiori di 65 anni
+
+    const calckmtot = calckm - 40 %
 }
-*/
+
+
+const rispostakm = `il prezzo è di ${calckmtot}`;
+
+document.getElementById('train_id').innerHTML = rispostakm;
