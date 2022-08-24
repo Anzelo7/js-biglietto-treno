cbf5ee1dabbac7ae5918121f3331881a4f619d0d
Anzelo7 <andrea.anzellotti7@gmail.com>
2022-08-24 10:19:04 +0200

    modifica

--- script.js
+++ script.js
@@ -9,20 +9,19 @@ const textage = prompt('Qual è la tua età?');
 const textageAsN = parseInt(textage);
 console.log('textageAsN', textageAsN);
 
+
 if (textageAsN < 18) {
     //applicare discount del 20% ai minorenni
 
-    const calckmtot = (calckm - 20 %);
-    document.getElementById('train_id').innerHTML = calckmtot;
+    calckmtot = (calckm / 100 * 20);
 
 } else if (textageAsN > 65) {
     //applicare discount del 40% ai maggiori di 65 anni
 
-    const calckmtot = (calckm - 40 %);
-    document.getElementById('train_id').innerHTML = calckmtot;
+    calckmtot = (calckm / 100 * 40);
 }
 
 
-const rispostakm = `Il prezzo è di ${calckmtot}`;
+const rispostakm = 'Il prezzo è di' + calckmtot;
 
 document.getElementById('train_id').innerHTML = rispostakm;
