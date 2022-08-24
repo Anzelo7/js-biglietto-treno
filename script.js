f4e1cde12124d91743f062ff6b8097745acb4df9
Anzelo7 <andrea.anzellotti7@gmail.com>
2022-08-24 17:55:47 +0200

    modifica prezzo totale biglietto

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
+console.log('JS OK!');
 
-const textkm = prompt('Quanti chilometri vuoi percorrere?');
-const textkmAsN = parseInt(textkm);
-console.log('textkmAsN', textkmAsN);
-const calckm = textkmAsN * 0.21;
+const messageHTMLElement = document.getElementById('message');
 
+//richiesta km
+const textkm = parseInt(prompt('Quanti chilometri vuoi percorrere?'));
 
-const textage = prompt('Qual è la tua età?');
-const textageAsN = parseInt(textage);
-console.log('textageAsN', textageAsN);
+//richiesta età
+const textage = parseInt(prompt('Qual è la tua età?'));
 
+if (!isNaN(textkm) && !isNaN(textage)) {
+    console.log('kmTrip e age sono numeri');
+    // calcolo costo totale  
 
-if (textageAsN < 18) {
-    //applicare discount del 20% ai minorenni
+    let calckm = textkm * 0.21;
+    let calckmtot = 0;
 
-    calckmtot = (calckm / 100 * 20);
+    if (textage < 18) {
+        //applicare discount del 20% ai minorenni
 
-} else if (textageAsN > 65) {
-    //applicare discount del 40% ai maggiori di 65 anni
+        calckmtot = (calckm / 100 * 20);
 
-    calckmtot = (calckm / 100 * 40);
+    } else if (textage > 65) {
+        //applicare discount del 40% ai maggiori di 65 anni
+
+        calckmtot = (calckm / 100 * 40);
+    }
+
+    calckm = calckm - calckmtot;
+
+    // output 
+    const message = '<p> Il costo del biglietto è ' + calckm.toFixed(2) + ' euro </p>';
+    console.log(message);
+    messageHTMLElement.innerHTML = message;
+} else {
+    messageHTMLElement.innerHTML = 'Dati inseriti in modo errato';
 }
-
-
-const rispostakm = 'Il prezzo è di' + calckmtot;
-
-document.getElementById('train_id').innerHTML = rispostakm;
